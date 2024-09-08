63aef8b8d7c8c1205ef289942251776f7ea9c0ee
suvigyachouhan06 <chouhansuvigya@gmail.com>
2024-09-08 20:11:54 +0530

    8sept eve

--- src/Components/header/dropBox/Dropbox.jsx
+++ src/Components/header/dropBox/Dropbox.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Button from '@mui/material/Button'
 import { FaAngleDown } from "react-icons/fa6";
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import { IoIosSearch } from "react-icons/io";
+ 
 import "./dropbox.css"
 const Dropbox = () => {
-  return (
+
   
+  return (
+  <>
       <Button className='countryDrop'>
             <div className="info d-flex flex-column">
                 <span className='location'>Your Location</span>
@@ -13,7 +19,15 @@ const Dropbox = () => {
             <span className='ms-auto'><FaAngleDown/></span>
         
     </Button>
-    
+    <Dialog open={true} className='locationModal '>
+    <DialogTitle>Choose Your Delivery Location</DialogTitle>
+    <p>Enter your address and we will specify the offer for your area.</p>
+    <div className="headerSearch w-100">
+        <input type="text" placeholder='Search your Area..' />
+        <Button><IoIosSearch/></Button>
+    </div>
+    </Dialog>
+  </>  
   )
 }
 
--- src/Components/header/navigation/Navigation.jsx
+++ src/Components/header/navigation/Navigation.jsx
@@ -3,15 +3,22 @@ import Button from '@mui/material/Button'
 import { IoIosMenu } from "react-icons/io";
 import { FaAngleDown } from "react-icons/fa";
 import { IoHomeOutline } from "react-icons/io5";
+import { GiClothes } from "react-icons/gi";
+import { FaMobileAlt } from "react-icons/fa";
+import { GiCakeSlice } from "react-icons/gi";
+import { MdOutlineLocalGroceryStore } from "react-icons/md";
+import { ImBlog } from "react-icons/im";
+import { IoIosContact } from "react-icons/io";
+
 import "./navigation.css"
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <nav>
-    <div className="container">
+    <div className="container my-4">
     <div className="row">
-      <div className="col-sm-3 navPart1">
+      <div className="col-sm-2 navPart1">
         <Button className='allCatTab align-items-center'>
           <span className='icon1 me-2'><IoIosMenu/></span>
           <span class='text'>ALL CATEGORIES</span>
@@ -19,15 +26,15 @@ const Navigation = () => {
         </Button>
       </div>
 
-      <div className="col-sm-9 navPart2 d-flex align-items-center ">
+      <div className="col-sm-10 navPart2 d-flex align-items-center ">
         <ul className="list list-inline ms-alto">
             <li className="list-inline-item"><Link to="/"><IoHomeOutline/> &nbsp;HOME</Link></li>
-            <li className="list-inline-item"><Link to="/">FASHION</Link></li>
-            <li className="list-inline-item"><Link to="/">ELECTRONICS</Link></li>
-            <li className="list-inline-item"><Link to="/">BAKERY</Link></li>
-            <li className="list-inline-item"><Link to="/">GROCERY</Link></li>
-            <li className="list-inline-item"><Link to="/">BLOG</Link></li>
-            <li className="list-inline-item"><Link to="/">CONTACT US</Link></li>
+            <li className="list-inline-item"><Link to="/"><GiClothes/>FASHION</Link></li>
+            <li className="list-inline-item"><Link to="/"><FaMobileAlt/>ELECTRONICS</Link></li>
+            <li className="list-inline-item"><Link to="/"><GiCakeSlice/>BAKERY</Link></li>
+            <li className="list-inline-item"><Link to="/"><MdOutlineLocalGroceryStore/>GROCERY</Link></li>
+            <li className="list-inline-item"><Link to="/"><ImBlog/>BLOG</Link></li>
+            <li className="list-inline-item"><Link to="/"><IoIosContact/>CONTACT US</Link></li>
         </ul>
       </div>
       </div>
